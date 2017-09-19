9d51261cb2b018c9a005ba11276796399a0a48e2
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-09-19 17:39:01 -0300

    issue #5-funciones básicas

---
+++ src/Pasajero.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mibici;
+
+class Pasajero implements PasajeroInterface{
+    
+    public $MoviID;
+    public $QuantityOfBikes;
+    
+    public function __construct(){
+          $this->MoviID=$this->obtenermoviid;
+          
+          
+          }
+    
+    
+    public function obtenermoviid(MoviInterface $movi){
+          
+          
+          
+          }
+    
+    
+    
+    }
