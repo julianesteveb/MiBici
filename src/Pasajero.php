e632531b16bc7fe933d1181fc4ecc0ddcad197c9
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-09-26 17:27:59 -0300

    Update Pasajero.php

--- src/Pasajero.php
+++ src/Pasajero.php
@@ -5,7 +5,6 @@ namespace Mibici;
 class Pasajero implements pasajeroInterface {
     
     public $moviId;
-    public $quantityOfBikes;
     
     public function __construct(Movi $movi) {
         $this->moviId=$movi->id;
