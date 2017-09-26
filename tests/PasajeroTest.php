cdf8a6fe5e51c370927aafde42bf3213c6050017
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-09-26 17:15:42 -0300

    camelCase, Espacios y modificaciones de assert

--- tests/PasajeroTest.php
+++ tests/PasajeroTest.php
@@ -5,9 +5,9 @@ namespace MiBici;
 use PHPUnit\Framework\TestCase;
 
 class PasajeroTest extends TestCase {
-public function testPasajero{
-$tarjetula = new Movi(41635174,123463463);
-$pas = new Pasajero($tarjetula);
-$this->assertEquals($pas->id,123463463);
-}
+    public function testPasajero{
+        $tarjetula = new Movi(41635174,123463463);
+        $pas = new Pasajero($tarjetula);
+        $this->assertEquals($pas->moviId,$tarjetula->id);
+    }
 }
