abcbfe890f0ca4f0c89de05340c6b3e48a7d5a63
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-09-26 17:19:23 -0300

    Update PasajeroTest.php

--- tests/PasajeroTest.php
+++ tests/PasajeroTest.php
@@ -5,7 +5,7 @@ namespace MiBici;
 use PHPUnit\Framework\TestCase;
 
 class PasajeroTest extends TestCase {
-    public function testPasajero {
+    public function testPasajero() {
         $tarjetula = new Movi(41635174,123463463);
         $pas = new Pasajero($tarjetula);
         $this->assertEquals($pas->moviId,$tarjetula->id);
