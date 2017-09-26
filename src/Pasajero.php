146979cb2b5717c4c12ce816024d0ea45bd22aa1
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-09-26 17:04:06 -0300

    Update Pasajero.php

--- src/Pasajero.php
+++ src/Pasajero.php
@@ -7,18 +7,10 @@ class Pasajero implements pasajeroInterface {
     public $moviId;
     public $quantityOfBikes;
     
-    public function __construct() {
-        $this->moviId=$this->obtenerMoviId;
-          
-          
-    }
-    
-    
-    public function obtenerMoviId(moviInterface $movi) {
-        return $movi->id;
-          
-    }
-    
-    
-    
+    public function __construct(Movi $movi) {
+        $this->moviId=$movi->id;
+             
+    } 
+   
+       
 }
